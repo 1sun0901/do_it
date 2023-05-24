faaf47ce6fcbd5989cf8ef58f313786d86a96727
1sun0901 <emuce@naver.com>
2023-05-24 22:02:41 +0900

    CVB로 블로그 포스트 상세 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import PostList, PostDetail
 
 urlpatterns = [
-    path('', views.index),
-    path('<int:pk>/', views.single_post_page),
+    path('', PostList.as_view()),
+    path('<int:pk>/', PostDetail.as_view()),
+    # path('', views.index),
+    # path('<int:pk>/', views.single_post_page),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,35 @@
 from django.shortcuts import render
-
 from blog.models import Post
+from django.views.generic import ListView, DetailView
 
+
+class PostList(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'blog/single_post_page.html'
 
 # Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+# def index(request):
+    # posts = Post.objects.all().order_by('-pk')
+    #
+    # return render(
+    #     request,
+    #     'blog/index.html',
+    #     {
+    #         'posts': posts,
+    #     }
+    # )
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
-
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post' : post,
-        }
-    )
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         'blog/single_post_page.html',
+#         {
+#             'post' : post,
+#         }
+#     )
