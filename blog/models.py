a2a47ea603b3d1ceb358ce1a36ca4a3a94280236
1sun0901 <emuce@naver.com>
2023-05-27 09:57:55 +0900

    작성자 정보 출력하기

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
 
     def __str__(self):
