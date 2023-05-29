d307d12ebf7b5a9c2304c6812119bda83d0c69c1
1sun0901 <emuce@naver.com>
2023-05-29 11:14:56 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
-from .views import PostList, PostDetail
 
 urlpatterns = [
+    path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>/', views.tag_page),
     path('category/<str:slug>/', views.category_page),
-    path('', PostList.as_view()),
-    path('<int:pk>/', PostDetail.as_view()),
+    path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),
     # path('', views.index),
     # path('<int:pk>/', views.single_post_page),
 ]
