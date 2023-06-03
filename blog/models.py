ee7e6c3279d1bae7f31583b27328d5b33f647c8f
1sun0901 <emuce@naver.com>
2023-06-03 20:37:46 +0900

    도커에 장고만 담기

--- blog/models.py
+++ blog/models.py
@@ -63,6 +63,12 @@ class Post(models.Model):
     def get_content_markdown(self):
         return markdown(self.content)
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://api.adorable.io/avatars/60/{self.author.username}.png'
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -75,3 +81,9 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://api.adorable.io/avatars/60/{ self.author.username }.png'
